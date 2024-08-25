dcbf5e4799c390aa09d42f907160420d24caabcf
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-08-25 15:48:38 +0100

    feat(routing): add basic routing for main pages
    Adds basic routing for the main pages of the website:
    
    - Home (HeroSection)
    - Hairstyles
    - Makeup
    - Nails
    - Eyelashes
    - Booking
    - About
    - Contact
    
    This allows users to navigate between these pages using the navigation menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Foot';
+import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
+import Hairstyles from './pages/Hairstyles';
+import Makeup from './pages/Makeup';
+import Nails from './pages/Nails';
+import Eyelashes from './pages/Eyelashes';
+import Booking from './pages/Booking';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Router>
       <Header />
-      <HeroSection />
-      {/* Other sections will go here */}
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/hairstyles" element={<Hairstyles />} />
+        <Route path="/makeup" element={<Makeup />} />
+        <Route path="/nails" element={<Nails />} />
+        <Route path="/eyelashes" element={<Eyelashes />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
